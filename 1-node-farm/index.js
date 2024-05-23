3c48fec8aa0836d3bbedbb9b2613e4b8648b0c99
sen <inarrituzanetti@outlook.com>
2024-05-23 00:03:23 -0600

    second commit

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+//__________________________________________________________________________________
+// FILES
 
 // // Blocking, synchronous way
 // const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
@@ -11,18 +16,47 @@ const fs = require('fs');
 // console.log('File written!');
 
 // Non-blocking, asynchronous way
-fs.readFile('./starter/txt/startttt.txt', 'utf-8', (err, data1) => {
-    if (err) return console.log('ERROR');
+// fs.readFile('./starter/txt/startttt.txt', 'utf-8', (err, data1) => {
+//     if (err) return console.log('ERROR');
 
-    fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
-            console.log(data3);
+//     fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
 
-            fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log("The file has been writen")
-            })
+//             fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log("The file has been writen")
+//             })
+//         });
+//     });
+// });
+// console.log('Will read file!');
+
+//_________________________________________________________________________________________
+// SERVER
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the OVERVIEW');
+    } else if(pathName === '/product'){
+        res.end('This is the PRODUCT');
+    } else if (pathName === '/api') {
+
+        fs.readFile(`${__dirname}/starter/dev-data/data.json`, 'utf-8', (err, data) => {
+            const productData = JSON.parse(data);
+            console.log(productData);
         });
-    });
+
+        res.end('API');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+        });
+        res.end('<h1>Page could not be found</h1>');
+    }
 });
-console.log('Will read file!');
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
